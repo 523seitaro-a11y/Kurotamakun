03442bf16e22d605c424b554c84c762566f86d01
523seitaro-a11y <523seitaro@gmail.com>
2026-03-06 00:20:06 +0900

    Small

--- WordGame/Assets/Script/SmallWorld/SmallMainCharacter.cs
+++ WordGame/Assets/Script/SmallWorld/SmallMainCharacter.cs
@@ -11,6 +11,8 @@ public class SmallMainChacter : MonoBehaviour
 
     private Vector3 _startPos;
 
+    private Vector3 _shrinkPos;
+
     void Awake()//Activeになった瞬間に一度だけ開始される処理
     {
         _startPos = transform.position;//Float用
@@ -23,17 +25,39 @@ public class SmallMainChacter : MonoBehaviour
 
     void OnEnable()//Activeになる度に開始される処理
     {
-
+        transform.localScale = new Vector3(10f,10f,10f);
+        StartCoroutine(Shrink());
     }
 
     void OnDisable()//非Activeになった瞬間に開始される処理（コルーチンの停止用）
     {
-        
+        StopCoroutine(Shrink());
+    }
+
+    IEnumerator Shrink()
+    {
+        Vector3 startScale = transform.localScale;
+        Vector3 targetScale = new Vector3(1.0f,1.0f,1.0f);
+
+        Vector3 startPosition = new Vector3(0f,12.0f,0f);
+        Vector3 targetPosition = Vector3.zero;
+
+        float elapsed = 0f;
+        float time = 1f;
+
+        while (elapsed < time)
+        {
+            transform.localScale = Vector3.Lerp(startScale, targetScale, elapsed / time);
+            _shrinkPos = Vector3.Lerp(startPosition, targetPosition, elapsed / time);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
     }
 
     void Float()//上下するアニメーション
     {
         float y = Mathf.Sin(Time.time * _speed) * _amplitude;
-        transform.position = _startPos + new Vector3(0, y, 0);
+        transform.position = _startPos + _shrinkPos + new Vector3(0, y, 0);
     }
 }
